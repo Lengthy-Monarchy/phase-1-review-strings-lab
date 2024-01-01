94d52f9fc0d3eda4151e0f9ee943d0161c9ee6b9
anne <anneirungu07@gmail.com>
2024-01-01 21:49:33 +0300

    completed

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Write your code in this file!
 const currentUser = 'Anne Irungu';
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
-console.log(welcomeMessage)
+const welcomeMessage = `Welcome to Flatbook!, ${currentUser}`;
+console.log(welcomeMessage);
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+const firstInitial = currentUser[0];
+const shortGreeting = 'Welcome ' + firstInitial;
+console.log(shortGreeting)
