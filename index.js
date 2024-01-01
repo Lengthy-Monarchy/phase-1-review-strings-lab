d7ba314700c534b26c021e477a1af513e2ad1ee9
anne <anneirungu07@gmail.com>
2024-01-01 22:03:23 +0300

    complete

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const welcomeMessage = `Welcome to Flatbook!, ${currentUser}`;
 console.log(welcomeMessage);
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 console.log(excitedWelcomeMessage);
-const firstInitial = currentUser[0];
+const firstInitial = currentUser .slice(0,5);
 const shortGreeting = 'Welcome ' + firstInitial;
 console.log(shortGreeting)
+console.log(firstInitial .length)
